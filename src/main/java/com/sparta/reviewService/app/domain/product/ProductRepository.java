10a2927843cdaa2337f4750c52bf8806c1eb3ffd
hizdev <gpfksdlrn@naver.com>
2024-12-13 09:35:02 +0900

    feat: 동시성 제어 비관적 락 추가

--- src/main/java/com/sparta/reviewService/app/domain/product/ProductRepository.java
+++ src/main/java/com/sparta/reviewService/app/domain/product/ProductRepository.java
@@ -1,9 +1,8 @@
 package com.sparta.reviewService.app.domain.product;
 
-import java.util.Optional;
-
 // 상품 데이터를 관리하는 인터페이스
 public interface ProductRepository {
-    Optional<Product> findById(Long productId); // 상품 ID로 조회
+    Product findById(Long productId); // 상품 ID로 조회
     void save(Product product);
+    Product findByIdWithPessimisticLock(Long productId);
 }
--- src/main/java/com/sparta/reviewService/app/infrastructure/repository/product/ProductJpaRepository.java
+++ src/main/java/com/sparta/reviewService/app/infrastructure/repository/product/ProductJpaRepository.java
@@ -1,7 +1,14 @@
 package com.sparta.reviewService.app.infrastructure.repository.product;
 
 import com.sparta.reviewService.app.domain.product.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id = :id")
+    Product findByIdWithPessimisticLock(@Param("id") Long id);
 }
